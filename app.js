725b86783315db5bdd530831e9ae4992401c1592
aarav <aravlead@gmail.com>
2025-12-20 12:56:16 +0530

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http=require('http');
-
 const app=http.createServer((req,res)=>{
 
+    const url=req.url;
+    const method=req.method;
 
-console.log(res);
-
+if(url==='/'){
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My first page</title><head>');
+res.write('<body> <form action ="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+res.write('</html>');
+return res.end();
+    }
 });
 console.log('http://localhost:3000')
 app.listen(3000);
