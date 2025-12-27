6824991343a81412f2c128f9becafb7ec828706b
aarav <aravlead@gmail.com>
2025-12-28 02:07:51 +0530

    Templating Engine-Pug

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const path = require('path');
 
-const http = require('http');
-const routes=require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app=http.createServer(routes);
+const app = express();
 
-console.log('http://localhost:3000')
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
 app.listen(3000);
