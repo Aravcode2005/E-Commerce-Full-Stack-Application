8fda4360e302bb5c609c39cf2ca570e9574c30f8
aarav <aravlead@gmail.com>
2026-01-02 15:27:28 +0530

    Ending Templating engines

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
@@ -18,3 +24,6 @@ router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect('/');
 });
+
+exports.routes = router;
+exports.products = products;
