a1907897cdc834ef74f48ef9af7693fcb5ad59c8
aarav <aravlead@gmail.com>
2026-02-19 22:33:20 +0530

    Fixing the app

--- util/database.js
+++ util/database.js
@@ -1,32 +1,29 @@
-const mongodb=require('mongodb');
-const MongoClient=mongodb.MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
 let _db;
-const mongoConnect=(callback)=>{
-MongoClient.connect('mongodb+srv://aravlead_db_user:ze7zntiuuNDhaSEL@cluster0.aohmc17.mongodb.net/shop?appName=Cluster0')
-.then(result=>{
-    console.log('Connected!');
-    _db=client.db()
-    callback(result);
-})
-.catch(err=>{
-console.log(err);
-throw err;
-});
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports=mongoConnect;
-
-const getDb=()=>{
-
-if(_db){
-
+const getDb = () => {
+  if (_db) {
     return _db;
-}
-throw 'No database found!';
+  }
+  throw 'No database found!';
+};
 
-}
-
-exports.getmongoConnect=mongoConnect;
-
-exports.getDb=getDb;
-
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
