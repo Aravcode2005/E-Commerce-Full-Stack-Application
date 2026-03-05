4a332acc8f0ce302c7240d78f576fe761a85db29
aarav <aravlead@gmail.com>
2026-03-05 11:05:51 +0530

    Updated Project Structure

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
 const path = require('path');
-const session=require('express-session');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const mongodbStore=require('connect-mongodb-session')(session);
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const dotenv = require('dotenv');
+dotenv.config();
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-const dotenv=require('dotenv');
-dotenv.config();
 const app = express();
-const store=new mongodbStore(
-uri:process.env.MONGO_DB_URI,
-collection:'sessions'
-);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_DB_URI,
+  collection: 'sessions'
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authRoutes=require('./routes/auth');
-const { MongoDBStore } = require('connect-mongodb-session');
+const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-secret:'ahajajjdjdjsdnxnn',
-resave:false,
-saveUninitialized:false,
-store:store
-}))
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
 });
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 app.use(errorController.get404);
-
 mongoose
   .connect(
-   process.env.MONGODB_URI
+    process.env.MONGO_DB_URI,
+    {dbName:'Ecommerce'}
   )
   .then(result => {
     User.findOne().then(user => {
@@ -63,3 +66,4 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
